604e777fe5d2e2c263ee049db534b1fee75185f3
Michael Fernandez <mikevfern@gmail.com>
2019-11-21 09:01:33 -0800

    IAuthRepository interface

---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        public Task<User> Register(User user, string password);
+        public Task<User> Login(string username, string password);
+        public Task<bool> UserExists(string username);
+    }
+}
