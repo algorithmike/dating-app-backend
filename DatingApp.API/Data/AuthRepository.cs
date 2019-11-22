acf639d33a4aca1103a9635692d02df43cf17030
Michael Fernandez <mikevfern@gmail.com>
2019-11-21 18:24:49 -0800

    AuthRepository methods to implement iAuthRepository interface

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,25 +1,38 @@
 ﻿using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext _context;
+
         private struct SaltyHash
         {
-            public byte[] salt;
-            public byte[] hash;
+            public byte[] salt, hash;
+
+            public SaltyHash(byte[] x, byte[] y)
+            {
+                salt = x;
+                hash = y;
+            }
         }
 
         public AuthRepository(DataContext context) { _context = context; }
 
         // Start - IAuthRepository implementation
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+            var saltyHash = new SaltyHash(user.PasswordSalt, user.PasswordHash);
+
+            if (user == null) return null;
+
+            if (!VerifyPasswordHash(password, saltyHash)) return null;
+
+            return user;
         }
 
         public async Task<User> Register(User user, string password)
@@ -34,9 +47,9 @@ namespace DatingApp.API.Data
             return user;
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new NotImplementedException();
+            return await _context.FindAsync<string>(username) != null;
         }
         // End - IAuthRepository implementation
 
@@ -49,6 +62,20 @@ namespace DatingApp.API.Data
                 retVal.hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 return retVal;
             }
+        }
+
+        private bool VerifyPasswordHash(string password, SaltyHash saltyHash)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA1(saltyHash.salt))
+            {
+                var computedHash = System.Text.Encoding.UTF8.GetBytes(password);
+
+                for(var x = 0; x++ < saltyHash.hash.Length;)
+                {
+                    if (computedHash[x] != saltyHash.hash[x]) return false;
+                }
+            }
+            return true;
         }
     }
 }
