17b2cc20ea18c5b7c50df6ce1d6173e4327ce8af
Michael Fernandez <mikevfern@gmail.com>
2019-11-23 10:15:08 -0800

    Fixed bug with hash verification

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.DTOs;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,8 +21,12 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo) { _repo = repo; }
+        public AuthController(IAuthRepository repo, IConfiguration config) {
+            _repo = repo;
+            _config = config;
+        }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
@@ -27,11 +36,10 @@ namespace DatingApp.API.Controllers
             userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDTO.Username))
-            {
                 return BadRequest("Username already exists.");
-            }
 
-            var user = await _repo.Register(new User { Username = userForRegisterDTO.Username }, userForRegisterDTO.Password);
+            var user = await _repo
+                .Register(new User { Username = userForRegisterDTO.Username }, userForRegisterDTO.Password);
 
             return StatusCode(201);
         }
@@ -39,7 +47,38 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
         {
-            throw new NotImplementedException();
+            var userFromRepo = await _repo
+                .Login(userForLoginDTO.Username.ToLower(), userForLoginDTO.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(0.5),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -37,9 +37,9 @@ namespace DatingApp.API.Data
 
         public async Task<User> Register(User user, string password)
         {
-            SaltyHash hash = CreatePasswordHash(password);
-            user.PasswordHash = hash.hash;
-            user.PasswordSalt = hash.salt;
+            SaltyHash saltyHash = CreatePasswordHash(password);
+            user.PasswordHash = saltyHash.hash;
+            user.PasswordSalt = saltyHash.salt;
 
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
@@ -55,22 +55,23 @@ namespace DatingApp.API.Data
 
         private SaltyHash CreatePasswordHash(string password)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA1())
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 SaltyHash retVal;
                 retVal.salt = hmac.Key;
                 retVal.hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
                 return retVal;
             }
         }
 
         private bool VerifyPasswordHash(string password, SaltyHash saltyHash)
         {
-            using(var hmac = new System.Security.Cryptography.HMACSHA1(saltyHash.salt))
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(saltyHash.salt))
             {
-                var computedHash = System.Text.Encoding.UTF8.GetBytes(password);
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
 
-                for(var x = 0; x++ < saltyHash.hash.Length;)
+                for(var x = 0; x < saltyHash.hash.Length; x++)
                 {
                     if (computedHash[x] != saltyHash.hash[x]) return false;
                 }
