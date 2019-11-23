f6b8f6a4e343e10fb27925d70621b2faff4bbf04
Michael Fernandez <mikevfern@gmail.com>
2019-11-22 18:19:10 -0800

    Added login dto and login() placeholder

---
+++ DatingApp.API/DTOs/UserForLoginDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace DatingApp.API.DTOs
+{
+    public class UserForLoginDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- DatingApp.API/DTOs/UserForRegisterDTO.cs
+++ DatingApp.API/DTOs/UserForRegisterDTO.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 10, ErrorMessage = "Password must be between 10 and 20 characters.")]
         public string Password { get; set; }
     }
 }
