6ee8a7d824f761f3caa692ca4f2c3f1b41800164
Michael Fernandez <mikevfern@gmail.com>
2019-12-01 11:46:56 -0800

    clear dev db, and create new migration

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DatingApp.API.Models
 {
@@ -13,5 +14,11 @@ namespace DatingApp.API.Models
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
